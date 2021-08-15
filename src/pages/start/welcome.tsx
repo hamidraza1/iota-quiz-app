3d43f4550f025f3e8b54b7a29ae74c57d642c639
hamidraza1 <hrazaa306@gmail.com>
2021-08-15 13:32:42 +0200

    added welcome page

--- src/pages/start/welcome.tsx
+++ src/pages/start/welcome.tsx
@@ -1,37 +1,18 @@
-import { useContext, useRef } from "react";
+import { Fragment, useContext, useRef } from "react";
 import QuizContext from "../../context/quizContext";
 import Container from "../../components/layout/container";
 import LayoutBody from "../../components/layout/layoutBody";
 import { useHistory } from "react-router-dom";
-import { EuiPageTemplate, EuiEmptyPrompt } from "@elastic/eui";
+import {
+  EuiButton,
+  EuiEmptyPrompt,
+  EuiPage,
+  EuiPageBody,
+  EuiPageContent,
+  EuiTitle,
+} from "@elastic/eui";
 
 const Welcome = () => {
-  const history = useHistory();
-
-  const user = useContext(QuizContext);
-
-  const usernameRef: any = useRef(null);
-
-  const onTestBeginHandler = () => {
-    user.newUser(usernameRef.current.value);
-
-    history.push("/quiz");
-  };
-
-  const content = (
-    <div>
-      <p>
-        Welcome to the IOTA Quiz portal. Here we will present you some questions
-        to test your general knowledge.{" "}
-      </p>
-      <p>
-        Each question will have a limited time and you can move back and forth
-        between questions.
-      </p>
-      <p>Good luck !! &#128640; &#128640;</p>
-    </div>
-  );
-
   return (
     // <Container heading="Welcome to Trivia Challenge">
     //   <EuiButton>send</EuiButton>
@@ -44,12 +25,42 @@ const Welcome = () => {
     //   </div>
     // </Container>
 
-    <EuiPageTemplate
-      template="centeredBody"
-      pageContentProps={{ paddingSize: "l" }}
-    >
-      <EuiEmptyPrompt title={<span>Welcome</span>} body={content} />
-    </EuiPageTemplate>
+    <EuiPage paddingSize="none" style={{ height: "100%" }}>
+      <EuiPageBody paddingSize="l">
+        <EuiPageContent
+          verticalPosition="center"
+          horizontalPosition="center"
+          paddingSize="none"
+        >
+          <EuiEmptyPrompt
+            iconType="package"
+            title={
+              <EuiTitle size="l">
+                <h1>Welcome.</h1>
+              </EuiTitle>
+            }
+            body={
+              <Fragment>
+                <p>
+                  Welcome to the IOTA Quiz portal. Here we will present you some
+                  questions to test your general knowledge.
+                </p>
+                <p>
+                  Each question will have a limited time and you can move back
+                  and forth between questions.
+                </p>
+                <p>Good luck !! &#128522; &#128522;</p>
+              </Fragment>
+            }
+            actions={
+              <EuiButton iconType="cheer" color="text" fill>
+                Lets get started!
+              </EuiButton>
+            }
+          />
+        </EuiPageContent>
+      </EuiPageBody>
+    </EuiPage>
   );
 };
 
