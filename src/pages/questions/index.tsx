b77f2e5ea3946be75f82b8da69ca4ed1f35e5a58
hamidraza1 <hrazaa306@gmail.com>
2021-08-15 15:24:22 +0200

    added sidebar

--- src/pages/questions/index.tsx
+++ src/pages/questions/index.tsx
@@ -1,12 +1,40 @@
-import { EuiHeader, EuiPage, EuiPageBody, EuiPageSideBar } from "@elastic/eui";
+import {
+  EuiHeader,
+  EuiListGroup,
+  EuiListGroupItem,
+  EuiPage,
+  EuiPageBody,
+  EuiPageSideBar,
+  EuiSpacer,
+} from "@elastic/eui";
 import React from "react";
+import css from "./index.module.scss";
 
 export type IQuestionsProps = {};
 
 const Questions: React.FC<IQuestionsProps> = ({}) => {
+  const list = Array(10)
+    .fill(0)
+    .map((_, item) => item + 1);
   return (
-    <EuiPage paddingSize="none" style={{ height: "100%" }}>
-      <EuiPageSideBar className="red"></EuiPageSideBar>
+    <EuiPage paddingSize="none" style={{ height: "100%", marginTop: "49px" }}>
+      <EuiPageSideBar className={css.sidebar}>
+        <EuiSpacer></EuiSpacer>
+        <EuiListGroup flush={false} bordered={false}>
+          {list.map((item) => (
+            <>
+              <EuiListGroupItem
+                size="m"
+                onClick={() => {
+                  console.log(item);
+                }}
+                label={"Question " + item}
+              />
+              <div className={css.border}></div>
+            </>
+          ))}
+        </EuiListGroup>
+      </EuiPageSideBar>
       <EuiPageBody className="blue"></EuiPageBody>
     </EuiPage>
   );
