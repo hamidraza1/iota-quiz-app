960111c6c4aa05f3068c28e1ca78af9ca861ef23
hamidraza1 <hrazaa306@gmail.com>
2021-08-15 12:28:04 +0200

    migrated to sass and add fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.css";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import "./App.scss";
 import QuizProvider from "./context/quizProvider";
-import Container from "./components/layout/container";
 import Quiz from "./pages/quiz/quiz";
 import Welcome from "./pages/start/welcome";
 import "@elastic/eui/dist/eui_theme_light.css";
+
 function App() {
   return (
     <QuizProvider>
       <Router basename={process.env.PUBLIC_URL}>
         <Switch>
-          <Route path="/" exact component={Container} />
-          <Route path="/Welcome" exact component={Welcome} />
+          <Route path="/" exact>
+            <Redirect to="/welcome" />
+          </Route>
+          <Route path="/welcome" exact component={Welcome} />
           <Route path="/quiz" exact component={Quiz} />
           {/*  <Route path='/timeline' component={TimeLine} /> */}
         </Switch>
--- src/pages/start/welcome.tsx
+++ src/pages/start/welcome.tsx
@@ -3,7 +3,7 @@ import QuizContext from "../../context/quizContext";
 import Container from "../../components/layout/container";
 import LayoutBody from "../../components/layout/layoutBody";
 import { useHistory } from "react-router-dom";
-import { EuiButton, EuiCallOut, EuiPanel } from "@elastic/eui";
+import { EuiPageTemplate, EuiEmptyPrompt } from "@elastic/eui";
 
 const Welcome = () => {
   const history = useHistory();
@@ -18,17 +18,38 @@ const Welcome = () => {
     history.push("/quiz");
   };
 
+  const content = (
+    <div>
+      <p>
+        Welcome to the IOTA Quiz portal. Here we will present you some questions
+        to test your general knowledge.{" "}
+      </p>
+      <p>
+        Each question will have a limited time and you can move back and forth
+        between questions.
+      </p>
+      <p>Good luck !! &#128640; &#128640;</p>
+    </div>
+  );
+
   return (
-    <Container heading="Welcome to Trivia Challenge">
-      <EuiButton>send</EuiButton>
-      <LayoutBody>
-        You will be presented with 10 true of false questions
-      </LayoutBody>
-      <input ref={usernameRef} type="text"></input>
-      <div>
-        <button onClick={onTestBeginHandler}>Begin</button>
-      </div>
-    </Container>
+    // <Container heading="Welcome to Trivia Challenge">
+    //   <EuiButton>send</EuiButton>
+    //   <LayoutBody>
+    //     You will be presented with 10 true of false questions
+    //   </LayoutBody>
+    //   <input ref={usernameRef} type="text"></input>
+    //   <div>
+    //     <button onClick={onTestBeginHandler}>Begin</button>
+    //   </div>
+    // </Container>
+
+    <EuiPageTemplate
+      template="centeredBody"
+      pageContentProps={{ paddingSize: "l" }}
+    >
+      <EuiEmptyPrompt title={<span>Welcome</span>} body={content} />
+    </EuiPageTemplate>
   );
 };
 
